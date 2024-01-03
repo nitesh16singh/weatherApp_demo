81b31c2267d2864e474229aae19fd26bd67f52ea
nitesh <nitesh16singh@users.noreply.github.com>
2024-01-03 21:05:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports= {
     dbUrl :'mongodb://mongo_db:27017',
     port: 3000,
     ttl: 24*60,
-    openWeatherKey: "c87b0dc01668b35a327c2d6d02328b9b",
+    openWeatherKey: "",
     geoTagUrl : "https://api.openweathermap.org/data/2.5/weather"
 };
